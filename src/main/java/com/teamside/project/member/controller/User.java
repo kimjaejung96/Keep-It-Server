8f7cec0e8c4a3e29ca7ad6341cb4138f4176677e
조준희 <46863381+RedJunHee@users.noreply.github.com>
2022-08-02 12:28:15 +0900

    CI/CD TEST - test API 추가

--- src/main/java/com/teamside/project/member/controller/User.java
+++ src/main/java/com/teamside/project/member/controller/User.java
@@ -1,8 +1,14 @@
 package com.teamside.project.member.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController(value = "/user")
 public class User {
-
+    @GetMapping(value = "/ping")
+    public ResponseEntity<String> ping() {
+        return ResponseEntity.status(HttpStatus.OK).body("pong!~");
+    }
 }
