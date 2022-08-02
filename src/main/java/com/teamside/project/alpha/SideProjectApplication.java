de037977b867c8eca005789a875679509e44fefa
조준희 <46863381+RedJunHee@users.noreply.github.com>
2022-08-02 13:10:59 +0900

    CI/CD TEST

--- src/main/java/com/teamside/project/alpha/SideProjectApplication.java
+++ src/main/java/com/teamside/project/alpha/SideProjectApplication.java
@@ -2,8 +2,10 @@ package com.teamside.project.alpha;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.teamside.project")
 public class SideProjectApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/teamside/project/member/controller/User.java
+++ src/main/java/com/teamside/project/member/controller/User.java
@@ -3,11 +3,13 @@ package com.teamside.project.member.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController(value = "/user")
+@RestController
+@RequestMapping("/user")
 public class User {
-    @GetMapping(value = "/ping")
+    @GetMapping("/ping")
     public ResponseEntity<String> ping() {
         return ResponseEntity.status(HttpStatus.OK).body("pong!~");
     }
