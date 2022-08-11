24852b5aa26029f5bcb42df82c1eb743c8d9f1e8
shCha <tmdgns277@gmail.com>
2022-08-11 20:44:40 +0900

    - sms 발송, 인증

--- src/main/java/com/teamside/project/alpha/member/repository/MemberRepo.java
+++ src/main/java/com/teamside/project/alpha/member/repository/MemberRepo.java
@@ -1,11 +1,15 @@
 package com.teamside.project.alpha.member.repository;
 
 import com.teamside.project.alpha.member.model.entity.MemberEntity;
+import com.teamside.project.alpha.member.model.enumurate.SignUpType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepo extends JpaRepository<MemberEntity, Long> {
     boolean existsByName(String name);
     boolean existsByPhone(String phone);
+    Optional<MemberEntity> findByPhoneAndType(String phone, SignUpType signUpType);
 }
--- src/main/java/com/teamside/project/alpha/member/service/AuthService.java
+++ src/main/java/com/teamside/project/alpha/member/service/AuthService.java
@@ -1,9 +1,14 @@
 package com.teamside.project.alpha.member.service;
 
+import com.teamside.project.alpha.common.exception.CustomException;
 import com.teamside.project.alpha.member.model.dto.JwtTokens;
+import com.teamside.project.alpha.member.model.dto.SmsAuthDto;
 
 public interface AuthService {
     JwtTokens getTokens(String mid);
     String createAccessToken(String mid);
     String createRefreshToken(String mid);
+    void saveSmsLog(String requestPhoneNum, String number) throws CustomException;
+    void checkAuthNum(SmsAuthDto smsAuthDto) throws CustomException;
+    JwtTokens checkMember(String phone) throws CustomException;
 }
