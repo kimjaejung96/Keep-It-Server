b12ebd1d27d0c0705c81d459d079282d79e17fdc
kimjaejung96 <jj.kim@okestro.com>
2022-08-19 10:54:23 +0900

    - jpa, jdbc 설정 변경

--- src/main/java/com/teamside/project/alpha/common/aop/repository/ApiLogRepo.java
+++ src/main/java/com/teamside/project/alpha/common/aop/repository/ApiLogRepo.java
@@ -2,9 +2,7 @@ package com.teamside.project.alpha.common.aop.repository;
 
 import com.teamside.project.alpha.common.aop.model.entity.ApiLogEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface ApiLogRepo extends JpaRepository<ApiLogEntity, Long> {
 
 }
