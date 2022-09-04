63155438097e957cedaa88d3506e5027bb6fb92d
shCha <tmdgns277@gmail.com>
2022-09-04 14:31:45 +0900

    - email 정규식 수정

--- src/main/java/com/teamside/project/alpha/common/model/constant/KeepitConstant.java
+++ src/main/java/com/teamside/project/alpha/common/model/constant/KeepitConstant.java
@@ -12,7 +12,7 @@ public class KeepitConstant {
 
 
     public static final String REGEXP_MEMBER_NAME = "^([-_A-Za-z0-9ㄱ-ㅎ가-힣\\\\.]){2,20}$";
-    public static final String REGEXP_EMAIL = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    public static final String REGEXP_EMAIL = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";
     public static final String REGEXP_PHONE = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$";
     public static final String REGEXP_AUTH_NUM = "^([0-9]{6})$";
     public static final String REGEXP_GROUP_NAME = "^(\\S)*(\\S){4,20}$";
