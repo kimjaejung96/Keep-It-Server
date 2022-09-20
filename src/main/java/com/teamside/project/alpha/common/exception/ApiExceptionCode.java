e6493a03593db64171aab2587ebb02ab52766aa5
kimjaejung96 <jj.kim@okestro.com>
2022-09-20 10:28:33 +0900

    [Add] 812 exception 추가 [Edit] 리뷰 생성 예외처리

--- src/main/java/com/teamside/project/alpha/common/exception/ApiExceptionCode.java
+++ src/main/java/com/teamside/project/alpha/common/exception/ApiExceptionCode.java
@@ -29,6 +29,7 @@ public enum ApiExceptionCode {
     DUPLICATE_ORD(809, "중복된 순서가 존재합니다."),
     GROUP_IS_NOT_MATCH(810, "내 그룹이 아닌 그룹이 존재합니다."),
     REVIEW_ALREADY_EXIST(811, "리뷰가 이미 존재합니다."),
+    PLACE_NOT_EXIST(812, "장소가 존재하지 않습니다."),
 
     SYSTEM_ERROR(999, "System Error"),
     ;
--- src/main/java/com/teamside/project/alpha/group/service/impl/ReviewServiceImpl.java
+++ src/main/java/com/teamside/project/alpha/group/service/impl/ReviewServiceImpl.java
@@ -7,20 +7,24 @@ import com.teamside.project.alpha.group.model.dto.ReviewDto;
 import com.teamside.project.alpha.group.model.entity.GroupEntity;
 import com.teamside.project.alpha.group.repository.GroupRepository;
 import com.teamside.project.alpha.group.service.ReviewService;
+import com.teamside.project.alpha.place.repository.PlaceRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class ReviewServiceImpl implements ReviewService {
     private final GroupRepository groupRepository;
+    private final PlaceRepository placeRepository;
 
-    public ReviewServiceImpl(GroupRepository groupRepository) {
+    public ReviewServiceImpl(GroupRepository groupRepository, PlaceRepository placeRepository) {
         this.groupRepository = groupRepository;
+        this.placeRepository = placeRepository;
     }
 
     @Override
     @Transactional
     public void createReview(ReviewDto.CreateReviewDto review) {
+        placeRepository.findByPlaceId(review.getPlaceId()).orElseThrow(() -> new CustomRuntimeException(ApiExceptionCode.PLACE_NOT_EXIST));
         GroupEntity group = groupRepository.findByGroupId(review.getGroupId()).orElseThrow(() -> new CustomRuntimeException(ApiExceptionCode.GROUP_NOT_FOUND));
         group.checkExistReview(review.getPlaceId());
 
