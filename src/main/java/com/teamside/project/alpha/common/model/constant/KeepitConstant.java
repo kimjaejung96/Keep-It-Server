acdd76c655b42dcc816ed0bee3176376848378fd
kimjaejung96 <jj.kim@okestro.com>
2022-09-24 14:46:07 +0900

    [Edit]멤버 아이디 밸리데이션 수정

--- src/main/java/com/teamside/project/alpha/common/model/constant/KeepitConstant.java
+++ src/main/java/com/teamside/project/alpha/common/model/constant/KeepitConstant.java
@@ -9,13 +9,12 @@ public class KeepitConstant {
 
 
 
-    public static final String REGEXP_MEMBER_NAME = "^([-_A-Za-z0-9ㄱ-ㅎ가-힣\\\\.]){2,20}$";
+    public static final String REGEXP_MEMBER_NAME = "^([-_A-Za-z0-9ㄱ-ㅎ가-힣[^\\\\.\\\\.]]){2,20}$";
     public static final String REGEXP_EMAIL = "^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$";
     public static final String REGEXP_PHONE = "^01(?:0|1|[6-9])(?:\\d{3}|\\d{4})\\d{4}$";
     public static final String REGEXP_AUTH_NUM = "^([0-9]{6})$";
     public static final String REGEXP_GROUP_NAME = "^\\S.*\\S$";
     public static final String REGEXP_GROUP_PASSWORD = "^[a-zA-Z0-9]{4,8}$";
-    public static final String REGEXP_GROUP_DESCRIPTION = "^.{10,200}$";
 
     public static final String REGEXP_EMOJI = "[^\\p{L}\\p{N}\\p{P}\\p{Z}]";
 
