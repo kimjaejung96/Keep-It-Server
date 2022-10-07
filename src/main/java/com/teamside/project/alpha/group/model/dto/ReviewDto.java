40be9fc01700b189693dbd532b88486886cebf4d
kimjaejung96 <jj.kim@okestro.com>
2022-10-07 16:07:16 +0900

    [Fix] review, daily 로그인 멤버 프로필 노출

--- src/main/java/com/teamside/project/alpha/group/model/dto/ReviewDto.java
+++ src/main/java/com/teamside/project/alpha/group/model/dto/ReviewDto.java
@@ -103,11 +103,13 @@ public class ReviewDto {
         ReviewDetail reviewsDetail;
         List<CommentDto> comments;
         String loginMemberName;
+        String loginMemberProfileUrl;
 
-        public ResponseReviewDetail(ReviewDetail reviewsDetail, List<CommentDto> comments, String loginMemberName) {
+        public ResponseReviewDetail(ReviewDetail reviewsDetail, List<CommentDto> comments, MemberEntity loginMember) {
             this.reviewsDetail = reviewsDetail;
             this.comments = comments;
-            this.loginMemberName = loginMemberName;
+            this.loginMemberName = loginMember.getName();
+            this.loginMemberProfileUrl = loginMember.getProfileUrl();
         }
     }
     @Getter
