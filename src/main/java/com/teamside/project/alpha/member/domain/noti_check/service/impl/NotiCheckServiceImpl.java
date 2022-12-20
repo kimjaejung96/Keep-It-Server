04568d447098e63b94ca244d897ff2797abf36e3
cha <tmdgns277@gmail.com>
2022-12-20 15:26:31 +0900

    [Fix] 알람설정 필터해제

--- src/main/java/com/teamside/project/alpha/member/domain/noti_check/service/impl/NotiCheckServiceImpl.java
+++ src/main/java/com/teamside/project/alpha/member/domain/noti_check/service/impl/NotiCheckServiceImpl.java
@@ -1,12 +1,19 @@
 package com.teamside.project.alpha.member.domain.noti_check.service.impl;
 
 import com.teamside.project.alpha.member.domain.noti_check.service.NotiCheckService;
+import com.teamside.project.alpha.member.repository.MemberRepo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NotiCheckServiceImpl implements NotiCheckService {
+    private final MemberRepo memberRepo;
+
+    public NotiCheckServiceImpl(MemberRepo memberRepo) {
+        this.memberRepo = memberRepo;
+    }
+
     @Override
     public void notiCheck() {
-
+        // 알람시간 + 1 ~ 현재 시간
     }
 }
